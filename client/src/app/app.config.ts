dfccc5acdc5fc2581198c2526c5f0f440983d22c
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-11-11 12:10:30 -0300

    Adicionada configuração inicial do projeto

---
+++ client/src/app/app.config.ts
@@ -0,0 +1,16 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+  ],
+};
---
+++ client/src/app/app.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  template: `<h1 class="font-bold">Hello World</h1>`,
+})
+export class App {}
