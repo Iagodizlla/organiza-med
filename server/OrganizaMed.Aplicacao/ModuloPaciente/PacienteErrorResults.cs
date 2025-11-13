00cbaeaef076fdb4a9611eb4cc9a1c78bc026e92
iagodizlla <iagoschlemper@gmail.com>
2025-11-13 11:37:33 -0300

    Resolvido bug ao editar pacientes na API

--- server/OrganizaMed.Aplicacao/ModuloPaciente/PacienteErrorResults.cs
+++ server/OrganizaMed.Aplicacao/ModuloPaciente/PacienteErrorResults.cs
@@ -4,13 +4,10 @@ namespace OrganizaMed.Aplicacao.ModuloPaciente;
 
 public abstract class PacienteErrorResults
 {
-    private static bool CpfDuplicado(Paciente paciente, IEnumerable<Paciente> pacientes)
+    public static Error CpfDuplicadoError(string cpf)
     {
-        return pacientes.Any(registro =>
-            registro.Id != paciente.Id &&
-            string.Equals(
-                registro.Cpf,
-                paciente.Cpf,
-                StringComparison.OrdinalIgnoreCase));
+        return new Error("CPF duplicado")
+            .CausedBy($"Um paciente com o CPF '{cpf}' já foi cadastrado")
+            .WithMetadata("ErrorType", "BadRequest");
     }
 }
